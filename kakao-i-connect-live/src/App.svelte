26289fbae19384d3979bb0810994d05547f237d8
Narastro <advice02@naver.com>
2022-06-06 15:20:40 +0900

    :tada: 새로운 방식으로 커넥트 라이브 달아보기 시작!

---
+++ kakao-i-connect-live/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import ConnectLive from '@connectlive/connectlive-web-sdk'
+</script>
+
+<main>
+  <h1>Hello Awesome Dale!</h1>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  #connect {
+    border: 0;
+    padding: 0.5rem;
+    color: white;
+    background-color: rgb(96 165 250 / var(--tw-bg-opacity));
+    cursor: pointer;
+  }
+
+  #disconnect {
+    border: 0;
+    padding: 0.5rem;
+    color: white;
+    background-color: rgb(248 113 113 / var(--tw-bg-opacity));
+    cursor: pointer;
+  }
+</style>
--- kakao-i-connect-live/vite.config.ts
+++ kakao-i-connect-live/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {adorableCSS} from 'adorable-css/vite'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [adorableCSS(), svelte()],
+  resolve: {
+    alias: {
+      src: resolve('src')
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 3000
+  }
+})
